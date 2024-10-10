6823722816db5b8cb90f2d58bdd7e4fe2468c8d2
Juan Galdo <80436377+JGaldo-beep@users.noreply.github.com>
2024-10-10 18:38:59 -0400

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+    
+    int t = 1;// cin >> t;
+    while (t--) {
+
+    }
+
+    // cerr<<"time taken : "<<(float)clock()/CLOCKS_PER_SEC<<" secs"<<endl;
+    return 0;
+}
